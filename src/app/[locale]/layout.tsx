213fb044a068d7e8977a80896099c2a463690c3b
Anton Kalkou <anton.kalkou@itechart-group.com>
2023-12-13 17:37:09 +0500

    feat(app): stop zooming when input on phones

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout(
   return (
     <html lang={locale}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
         <link rel="shortcut icon" href={Logo.src} />
       </head>
       <body className="h-[100vh]">
