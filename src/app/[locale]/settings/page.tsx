0628da2698c2108c2caf7ffd3684c05f85dbd6b5
Anton Kalkou <anton.kalkou@itechart-group.com>
2023-12-13 16:37:00 +0500

    fix(app): fix responsive design

--- src/app/[locale]/settings/page.tsx
+++ src/app/[locale]/settings/page.tsx
@@ -2,7 +2,7 @@ import Settings from './components/Settings/Settings';
 
 export default function SettingsPage() {
   return (
-    <div className="settings-page w-full">
+    <div className="settings-page w-full h-full">
       <Settings />
     </div>
   );
